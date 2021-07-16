568ccb9c9f6f3aaf66677bb4fb8a96b2a75e4cb0
dieungocbao <dieungocbao@gmail.com>
2021-07-16 22:14:04 +0700

    fix webpack path errors

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,46 +3,46 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
-    entry: path.resolve(__dirname, "./src/index.tsx"),
-    output: {
-        path: path.resolve(__dirname, "./build"),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: [".tsx", ".ts", ".js"]
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(ts|js)x?$/,
-                exclude: /node_modules/,
-                use: [
-                    {
-                        loader: 'babel-loader'
-                    }
-                ]
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/i,
-                type: 'asset/resource',
-            },
+  entry: path.resolve(__dirname, "../src/index.tsx"),
+  output: {
+    path: path.resolve(__dirname, "./build"),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
         ]
-    },
-
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, './public/index.html')
-        }),
-        new CopyPlugin({
-            patterns: [{ from: "source", to: "dest", noErrorOnMissing: true }],
-        })
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource'
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    }),
+    new CopyPlugin({
+      patterns: [{ from: "source", to: "dest", noErrorOnMissing: true }],
+    })
+  ]
 }
